2143543c0fe2b1f1b7b1c0726e92efa2828d1f77
crystalrealm-maker <chicworld@hotmail.com>
2026-01-26 21:03:33 +0000

    Adjusted enemy bullet speed to 1.8

--- src/controls.js
+++ src/controls.js
@@ -178,7 +178,7 @@ export function drawBullets(ctx) {
 // ============================
 bullets.enemyBullets.forEach((eb, i) => {
 
-    eb.y += 3.5;   // velocidade final ajustada
+    eb.y += 1.8;   // velocidade final REALMENTE justa
 
     ctx.shadowBlur = 25;
     ctx.shadowColor = "#ff0000";
